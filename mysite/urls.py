5740a7c47b3eba29431f194813bbae2ca10e3340
Leekyungsoo <dlrudtn0924@naver.com>
2017-09-11 11:23:21 +0900

    ~p.128

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,5 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^polls/$', views.index, name='index'),
+    url(r'^polls/(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^polls/(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^polls/(?P<question_id>\d+)/results/$', views.results, name='results'),
     url(r'^admin/', admin.site.urls),
 ]
