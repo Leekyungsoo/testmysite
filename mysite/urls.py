a283fa25003d3d2513643a8392b68582c2731caf
Leekyungsoo <dlrudtn0924@naver.com>
2017-09-11 18:08:47 +0900

    no message

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
 
-    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
